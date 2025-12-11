73f6135b2ec37bbf1ca75aa278a19144465538ac
kaiuwepeter <sidekix79@gmail.com>
2025-12-11 19:59:02 +0100

    Create KriemhildeTools-deDE.lua

---
+++ Locales/KriemhildeTools-deDE.lua
@@ -0,0 +1,8 @@
+-- KriemhildeTools
+-- Please use the Localization App on Curseforge to Update this
+-- https://legacy.curseforge.com/wow/addons/kriemhildetools/localization
+
+local LK = LibStub("AceLocale-3.0"):NewLocale("KriemhildeTools", "deDE", true)
+if not LK then return end
+
+--@localization(locale="deDE", format="lua_additive_table", namespace="Konfiguration", table-name="LK", handle-unlocalized="ignore")@
