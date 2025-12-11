0823c92aecf7c55e62ff2eed0cd4b635b6c97582
kaiuwepeter <sidekix79@gmail.com>
2025-12-11 20:04:04 +0100

    Create KriemhildeTools-enUS.lua

---
+++ Locales/KriemhildeTools-enUS.lua
@@ -0,0 +1,4 @@
+local LK = LibStub("AceLocale-3.0"):NewLocale("KriemhildeTools", "enUS")
+if not LK then return end
+  
+--@localization(locale="enUS", format="lua_additive_table", namespace="Konfiguration", table-name="LK", handle-unlocalized="ignore")@
